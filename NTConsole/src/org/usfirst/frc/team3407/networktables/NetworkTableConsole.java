3005c3078e68afcb51eaa61570406f8a4665bbd3
jsthompson49 <ljthompsons@yahoo.com>
2016-11-23 19:55:17 -0600

    NTConsole initial checkin
    PC Program to read NetworkTables  on the RoboRIO

---
+++ NTConsole/src/org/usfirst/frc/team3407/networktables/NetworkTableConsole.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team3407.networktables;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class NetworkTableConsole {
+
+	public static void main(String[] args) {
+
+		final int PORT = -1;
+		final String INPUT_KEY = "TestInput";
+		final String TABLE_NAME = "AccelerometerTest";
+		
+		System.out.println("LibraryPath=" + System.getProperties().getProperty("java.library.path"));
+		NetworkTable.setClientMode();
+	
+		// Implicilty sets IP address and 
+		NetworkTable.setTeam(3407);
+		if(PORT >= 0) {
+			NetworkTable.setPort(PORT);
+		}
+		
+		System.out.println("Port=" + PORT);
+				
+		NetworkTable table = NetworkTable.getTable(TABLE_NAME);
+		table.putString(INPUT_KEY, "Hello");
+		
+		int i = 0;
+		while(true) {
+			try {
+				i++;
+				//Object value = table.getNumber(STATUS_KEY, -1234567);
+				Object xValue = table.getNumber("Xval", -1234567);
+				Object yValue = table.getNumber("Yval", -1234567);
+				Object zValue = table.getNumber("Zval", -1234567);
+				System.out.println(" x=" + xValue+ " y=" + yValue+ " z=" + zValue);
+				
+				if((i % 2) == 0) {
+					System.out.println("TableContents=" + table);
+				}
+				Thread.sleep(3000);
+			}
+			catch (Exception e) {
+				System.err.println("ERROR: " + e.getMessage());
+			}
+			
+		}
+			
+	}
+}
